538db50f507dfb42b3c95db1341cc9c7d59de1af
addisalemtafere <addisalem12@gmail.com>
2022-03-03 23:01:03 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,38 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   form: FormGroup;
-
+  auto: any[] = [];
   constructor(private fb: FormBuilder) {
     this.form = fb.group({
       integrationTypeId: [],
-      enabled:[],
-      warehouseAutomationParameters: fb.array([])
-    })
+      enabled: [],
+      warehouseAutomationParameters: fb.array([]),
+    });
   }
-
 
   addNewAddressGroup() {
     const add = this.form.get('warehouseAutomationParameters') as FormArray;
-    add.push(this.fb.group({
-      name: [],
-      value: []
-    }))
+    add.push(
+      this.fb.group({
+        name: [],
+        value: [],
+      })
+    );
   }
 
   deleteAddressGroup(index: number) {
     const add = this.form.get('warehouseAutomationParameters') as FormArray;
-    add.removeAt(index)
+    add.removeAt(index);
+  }
+
+
+  save(){
+    this.auto.push(this.form.value);
   }
 }
-
-
-
